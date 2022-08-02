224ce8f715df114b788ca2e409fa46e95e9f2eda
longbowlu <zl9394@gmail.com>
2022-08-02 16:36:38 -0700

    clean up

--- crates/sui-faucet/src/faucet/simple_faucet.rs
+++ crates/sui-faucet/src/faucet/simple_faucet.rs
@@ -3,7 +3,7 @@
 
 use anyhow::anyhow;
 use async_trait::async_trait;
-use sui::client_commands::WalletContext;
+use sui::client_commands::{SuiClientCommands, WalletContext};
 use sui_json_rpc_types::{SuiExecutionStatus, SuiParsedObject};
 use sui_types::{
     base_types::{ObjectID, SuiAddress, TransactionDigest},
@@ -36,6 +36,14 @@ impl SimpleFaucet {
             .active_address()
             .map_err(|err| FaucetError::Wallet(err.to_string()))?;
         info!("SimpleFaucet::new with active address: {active_address}");
+
+        // Sync to have the latest status
+        SuiClientCommands::SyncClientState {
+            address: Some(active_address),
+        }
+        .execute(&mut wallet)
+        .await
+        .map_err(|err| FaucetError::Wallet(format!("Fail to sync client state: {}", err)))?;
 
         let mut coins = wallet
             .gas_objects(active_address)
